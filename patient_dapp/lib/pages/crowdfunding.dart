7405e81533030aa695a59e5998262c6838b88c1a
Gagandeep <gagandeepkaur1523@gmail.com>
2022-11-18 19:01:46 +0530

    Initial commit

---
+++ patient_dapp/lib/pages/crowdfunding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Crowdfunding extends StatefulWidget {
+  const Crowdfunding({super.key});
+
+  @override
+  State<Crowdfunding> createState() => _CrowdfundingState();
+}
+
+class _CrowdfundingState extends State<Crowdfunding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
