475a2a2b30c761affea2526b49ff810525be719f
Gagandeep <gagandeepkaur1523@gmail.com>
2022-11-18 22:01:29 +0530

    returned container directly

--- patient_dapp/lib/pages/patent_card.dart
+++ patient_dapp/lib/pages/patent_card.dart
@@ -5,41 +5,38 @@ class PatentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          // debugShowCheckedModeBanner: false,
-          // title: 'Flutter Demo',
-          // themeMode: ThemeMode.light,
-          // theme: const NeumorphicThemeData(
-          //   baseColor: Color.fromARGB(255, 121, 28, 28),
-          //   lightSource: LightSource.topLeft,
-          //   depth: 10,
-          // ),
-          // darkTheme: const NeumorphicThemeData(
-          //   baseColor: Color.fromARGB(255, 76, 163, 36),
-          //   lightSource: LightSource.topLeft,
-          //   depth: 6,
-          // ),
-          body: Container(
-        height: 100,
-        child: Neumorphic(
-          style: NeumorphicStyle(
-              shape: NeumorphicShape.concave,
-              boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
-              depth: 8,
-              lightSource: LightSource.topLeft,
-              color: Colors.grey),
-          child: Column(
-            children: const [
-              Text(
-                'Hello World !!',
-                style: TextStyle(color: Color.fromARGB(255, 150, 255, 150)),
-              )
-            ],
-          ),
+    return
+        // debugShowCheckedModeBanner: false,
+        // title: 'Flutter Demo',
+        // themeMode: ThemeMode.light,
+        // theme: const NeumorphicThemeData(
+        //   baseColor: Color.fromARGB(255, 121, 28, 28),
+        //   lightSource: LightSource.topLeft,
+        //   depth: 10,
+        // ),
+        // darkTheme: const NeumorphicThemeData(
+        //   baseColor: Color.fromARGB(255, 76, 163, 36),
+        //   lightSource: LightSource.topLeft,
+        //   depth: 6,
+        // ),
+        Container(
+      height: 100,
+      child: Neumorphic(
+        style: NeumorphicStyle(
+            shape: NeumorphicShape.concave,
+            boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+            depth: 8,
+            lightSource: LightSource.topLeft,
+            color: Colors.grey),
+        child: Column(
+          children: const [
+            Text(
+              'Hello World !!',
+              style: TextStyle(color: Color.fromARGB(255, 150, 255, 150)),
+            )
+          ],
         ),
-      )),
+      ),
     );
   }
 }
