9264eb96d38ec422e385dab6beb5bc3438d4412d
Gagandeep <gagandeepkaur1523@gmail.com>
2022-11-19 09:53:05 +0530

    Fixed overflow error

--- patient_dapp/lib/widgets/crowdfunding_card.dart
+++ patient_dapp/lib/widgets/crowdfunding_card.dart
@@ -28,88 +28,90 @@ class _CFCardState extends State<CFCard> {
         ),
         child: Container(
           margin: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'PATENT NAME',
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
-                style: TextStyle(
-                  fontSize: 10,
+          child: Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'PATENT NAME',
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'White Paper',
-                  style: TextStyle(color: Colors.blue, fontSize: 10),
+                const SizedBox(
+                  height: 5,
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: const [
-                  Text(
-                    'Uses Electricity',
-                    style: TextStyle(
-                      fontSize: 10,
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: const Text(
+                    'White Paper',
+                    style: TextStyle(color: Colors.blue, fontSize: 10),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: const [
+                    Text(
+                      'Uses Electricity',
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Text(
-                    'Uses Fuel',
-                    style: TextStyle(
-                      fontSize: 10,
+                    SizedBox(
+                      width: 15,
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: TextField(
-                          controller: textController,
-                          keyboardType: TextInputType.number,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter the Amount',
-                          ),
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: const Text(
-                              'Confirm',
-                              style: TextStyle(color: Colors.black),
+                    Text(
+                      'Uses Fuel',
+                      style: TextStyle(
+                        fontSize: 10,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: TextField(
+                            controller: textController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: 'Enter the Amount',
                             ),
                           ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 36, 35, 35),
-                ),
-                child: const Text('Fund'),
-              )
-            ],
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text(
+                                'Confirm',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 36, 35, 35),
+                  ),
+                  child: const Text('Fund'),
+                )
+              ],
+            ),
           ),
         ),
       ),
